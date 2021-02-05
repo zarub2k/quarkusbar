e7555864396e2e993a65f1bc9e4ecdd013f92576
Tham <zarub2k@gmail.com>
2021-02-05 14:14:24 +0530

    initial commit

---
+++ src/main/java/xyz/tham/quarkusbar/GreetingResource.java
@@ -0,0 +1,16 @@
+package xyz.tham.quarkusbar;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/greet")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
