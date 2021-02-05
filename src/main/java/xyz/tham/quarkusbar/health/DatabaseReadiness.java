2f782b7686747cc62cf774aba8dab6beb6ad1728
Tham <zarub2k@gmail.com>
2021-02-05 15:11:35 +0530

    Health check is included

---
+++ src/main/java/xyz/tham/quarkusbar/health/DatabaseReadiness.java
@@ -0,0 +1,17 @@
+package xyz.tham.quarkusbar.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Readiness
+@ApplicationScoped
+public class DatabaseReadiness implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse
+            .up("Database Readiness");
+    }
+}
---
+++ src/main/java/xyz/tham/quarkusbar/health/SimpleHealthCheck.java
@@ -0,0 +1,17 @@
+package xyz.tham.quarkusbar.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Liveness
+@ApplicationScoped
+public class SimpleHealthCheck implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse
+            .up("Simple Health Checker");
+    }
+}
